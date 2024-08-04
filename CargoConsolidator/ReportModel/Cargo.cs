9aeafb26e37c044c7d79995681cb9422a1974fe9
IgorMoiseev1988 <I.Moiseev.1988@yandex.ru>
2024-08-04 18:00:48 +0300

    Resolve issue #1

---
+++ CargoConsolidator/ReportModel/Cargo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoConsolidator.ReportModel
+{
+    internal class Cargo
+    {
+        public int Id { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
---
+++ CargoConsolidator/ReportModel/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoConsolidator.ReportModel
+{
+    internal class Storage
+    {
+        public int Id { get; set; }
+        public List<Cargo> Cargos { get; set; }
+    }
+}
